a9df73aa56a29b6a81656edbf902d60c48a4108c
plusmancn <plusmancn@gmail.com>
2017-02-10 00:56:13 +0800

    fix: clean code

--- example/app/index.js
+++ example/app/index.js
@@ -12,37 +12,13 @@ const yargv = require('yargs').argv;
 // 变量可选值：development, production
 process.env.NODE_ENV = (yargv.env || 'development').toLowerCase();
 
-let server = require('../../').start({
+require('../../').start({
     baseDir: __dirname,
     cloverEnv: process.env.NODE_ENV
 });
 
-// 启动 socket.io
-const uuid = require('uuid');
-const io = require('socket.io')(server);
-const userSocketIdMap = new Map();
-const socketIdUserMap = new Map();
-
-io.on('connection', function (socket) {
-    socket.on('peerMessage', function (data) {
-        let to = userSocketIdMap.get(data.to);
-        let from = userSocketIdMap.get(data.from);
-        socket
-            .to(to.socketId)
-            .emit('message', {
-                content: data.content,
-                avatar: from.avatar,
-                uuid: data.uuid || uuid.v4()
-            });
-    });
-
-    socket.on('registry', function (data) {
-        userSocketIdMap.set(data.userId, data);
-        socketIdUserMap.set(data.socketId, data);
-
-        console.log('new User', data);
-    });
-});
+// 启动 socket 服务
+require('./socket/index.js');
 
 process.on('uncaughtException', (err) => {
     console.error(err);
